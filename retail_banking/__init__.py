4d4c31c3815ded2788e05a4435337f9f543e488c
Saurabh Singh <saurabhsingh1@ternaengg.ac.in>
2020-06-16 20:38:57 +0530

    Added Database

--- retail_banking/__init__.py
+++ retail_banking/__init__.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 config = Config()
 
 app.config['SECRET_KEY'] = config.secret_key
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+db : SQLAlchemy = SQLAlchemy(app)
 
 from retail_banking import routes # Strictly place this import here only
--- retail_banking/models.py
+++ retail_banking/models.py
@@ -1,25 +1,26 @@
 from retail_banking import db
+from flask_sqlalchemy.utils import sqlalchemy
+
+class Employee(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    name = db.Column(db.String(80), nullable=False)
+    dob = db.Column(db.String(80), nullable=False)
+    is_executive = db.Column(db.Boolean(), default=False)
 
 
-class Executive(db.Model):
-    ...
+# class Account(db.Model):
+#     ...
 
 
-class Cashier(db.Model):
-    ...
+# class Transaction(db.Model):
+#     ...
 
 
-class Account(db.Model):
-    ...
+# class Customer(db.Model):
+#     ...
 
 
-class Transaction(db.Model):
-    ...
-
-
-class Customer(db.Model):
-    ...
-
-
-class Activity(db.Model):
-    ...
+# class Activity(db.Model):
+#     ...
